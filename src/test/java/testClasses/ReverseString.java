34cfaed0572fda5bf7bcdf3e00fb4a57a0882cd4
Shiva <shivabadiger7@gmail.com>
2026-05-29 15:57:19 +0530

    added string reverse program and executing using maven test

---
+++ src/test/java/testClasses/ReverseString.java
@@ -0,0 +1,18 @@
+package testClasses;
+
+import org.testng.annotations.Test;
+
+public class ReverseString {
+	
+	@Test
+	public void ReverserStringMethod() {
+		String str="Selenium";
+		String rev="";
+		for(int i=str.length()-1; i>=0; i--) {
+			rev = rev+str.charAt(i);
+		}
+		
+		System.out.println(rev);
+	}
+
+}
